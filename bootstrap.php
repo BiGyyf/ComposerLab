1d1c920378e5c9bb0e2432184d453e6cd32570af
49943222 <49943222@gm.nfu.edu.tw>
2016-01-22 21:45:36 +0800

    my composer lab

---
+++ bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+require '../MyFirstComposerProject/vendor/autoload.php';
+define('DB_HOST', 'localhost');
+define('DB_PORT', 3306);
+define('DB_DATABASE', 'database');
+define('DB_CHARSET', 'utf8');
+define('DB_USERNAME', 'homestead');
+define('DB_PASSWORD', 'secret');
